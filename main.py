cbacbaabe9be47ac2bb31afa3c343e149c60d7f4
Opeyemi Aina <ainaopeyemi53@gmail.com>
2025-06-08 11:04:16 +0100

    prepare base model completed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.cnnClassifier import logger
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-#from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 #from src.cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
 #from src.cnnClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
 
@@ -15,6 +15,27 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
